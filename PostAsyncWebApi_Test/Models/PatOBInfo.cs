60fec63bef0649cf54ca02bfaaf972b9bf927eea
Tahir Alvi <engtahiralvi@gmail.com>
2019-04-02 21:09:10 +0500

    Update the code, clean it and make it more useable
    Update the code, clean it and make it more useable

---
+++ PostAsyncWebApi_Test/Models/PatOBInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace PostAsyncWebApi_Test.Models
+{
+    public class PatOBInfo
+    {
+        public int acokey { get; set; }
+        public int practiceid { get; set; }
+        public string userid { get; set; }
+        public string searchtext { get; set; }
+        public int option { get; set; }
+        public int viewall { get; set; }
+    }
+}
---
+++ PostAsyncWebApi_Test/Models/PostDataModel.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace PostAsyncWebApi_Test.Models
+{
+    public static class PostDataModel
+    {
+        public static List<PatOBInfo> PostAsyncWebApi_Test(PatOBInfo product)
+        {
+            List<PatOBInfo> list = new List<PatOBInfo>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://internal.premierphc360.com/CCMAPI");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                StringContent content = new StringContent(JsonConvert.SerializeObject(product), System.Text.Encoding.UTF8, "application/json");
+                // HTTP POST
+                HttpResponseMessage response = client.PostAsync("https://internal.premierphc360.com/CCMAPI/api/Patient/PostAsyncWebApi_Test", content).Result;
+                string data1 = response.Content.ReadAsStringAsync().Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = response.Content.ReadAsStringAsync().Result;
+                    list = JsonConvert.DeserializeObject<List<PatOBInfo>>(data);
+                }
+            }
+            return list;
+        }
+    }
+}
